ed5ed0ae83847f5041484f3a1da84a2dbb3921ed
Antonino <a.bonanno@gmail.com>
2023-12-29 10:43:17 +0000

    Adding new cli funcionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -6,3 +6,9 @@ def wiki(name="Monza", lenght=1):
 
     my_wiki = wikipedia.summary(name, lenght)
     return my_wiki
+
+def search_wiki(name):
+    """Search Wikipedia for Names"""
+
+    results = wikipedia.search(name)
+    return results
